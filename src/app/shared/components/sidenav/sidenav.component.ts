ac2a3397106331d00e9a36eb7cb5626bd50c8335
Skrpion <nestor.martinez.c@uniautonoma.edu.co>
2023-09-28 17:39:40 -0500

    feat: cerrar sesión

--- src/app/shared/components/sidenav/sidenav.component.ts
+++ src/app/shared/components/sidenav/sidenav.component.ts
@@ -1,22 +1,32 @@
 import { MediaMatcher } from '@angular/cdk/layout';
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
-export class SidenavComponent {
+export class SidenavComponent implements OnInit {
 
-  mobileQuery: MediaQueryList; // Responsive media query
+  private media = inject(MediaMatcher);
+  private keyCloakSrv = inject(KeycloakService);
+
+  username = '';
+  mobileQuery!: MediaQueryList; // Responsive media query
   menuNav = [
     {name: "Home", route: "home", icon: "home"},
     {name: "Categorías", route: "categories", icon: "category"},
     {name: "Productos", route: "products", icon: "add_shopping_cart"}
   ]
-  constructor(media: MediaMatcher) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+
+  ngOnInit(): void {
+    this.mobileQuery = this.media.matchMedia('(max-width: 600px)');
+    this.username = this.keyCloakSrv.getUsername();
   }
 
+  logout(): void {
+    this.keyCloakSrv.logout();
+  }
 
 }
